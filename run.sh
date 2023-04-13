742d02924c8881ea033d8327ec75fc25c10f00e9
shenyangHuang <shenyang.huang@mail.mcgill.ca>
2023-04-13 14:55:05 -0400

    update the code

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --partition=main  #unkillable #main #long
-#SBATCH --output=fm_linkpred.txt 
-#SBATCH --error=fm_linkprederror.txt   
+#SBATCH --partition=unkillable  #unkillable #main #long
+#SBATCH --output=tgn_fm.txt #fm_linkpred.txt 
+#SBATCH --error=tgn_fmerror.txt   
 #SBATCH --cpus-per-task=4                     # Ask for 4 CPUs
 #SBATCH --gres=gpu:rtx8000:1                  # Ask for 1 titan xp
 #SBATCH --mem=32G                             # Ask for 32 GB of RAM
@@ -12,4 +12,5 @@ module load python/3.9
 source $HOME/tgbenv/bin/activate
 
 pwd
-CUDA_VISIBLE_DEVICES=0 python examples/nodeproppred/tgn_linkpred.py 
+# CUDA_VISIBLE_DEVICES=0 python examples/nodeproppred/tgn_linkpred.py 
+CUDA_VISIBLE_DEVICES=0 python examples/nodeproppred/tgn.py 
