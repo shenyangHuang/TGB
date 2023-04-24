92163fe3bb430fca0dc6d761d947dceece560890
Farimah Poursafaei <fpour@narval3.narval.calcul.quebec>
2023-04-24 13:49:11 -0400

    utility

--- examples/edgeprediction/edgepred_utils.py
+++ examples/edgeprediction/edgepred_utils.py
@@ -7,17 +7,33 @@ Utility functions required for the task of link prediction
 
 import numpy as np
 import torch
-
+import random
+import os
 
 
 # general parameters 
 K = 10  # for computing metrics@k
 
 
+def set_random_seed(seed):
+  """
+  set random seed
+  """
+  torch.manual_seed(seed)
+  torch.cuda.manual_seed_all(seed)
+  torch.backends.cudnn.deterministic = True
+  torch.backends.cudnn.benchmark = False
+  np.random.seed(seed)
+  random.seed(seed)
+  os.environ['PYTHONHASHSEED'] = str(seed)
 
-def gen_eval_set_for_batch(src, dst, min_dst_idx, max_dst_idx):
+
+def gen_eval_set_for_batch_multi_pos(src, dst, min_dst_idx, max_dst_idx):
     """
     generate the evaluation set of edges for a batch of positive edges
+    in this setting, if there are more than one positive edges with the same source node, both will be considered together
+    and they are evaluated against all possible relevant negative edges
+    NOTE: the key of the returned dictionary is the source node of the positive edges
     """
     pos_src = src.cpu().numpy()
     pos_dst = dst.cpu().numpy()
@@ -41,6 +57,34 @@ def gen_eval_set_for_batch(src, dst, min_dst_idx, max_dst_idx):
 
     return edges_per_node
 
+
+def gen_eval_set_for_batch_one_pos(src, dst, min_dst_idx, max_dst_idx):
+    """
+    generate the evaluation set of edges for a batch of positive edges
+    in this setting, each positive edge is evaluated agains all possible relevant negative edges
+    thus, if there are multiple positive edges with the same source node, each one is evaluated individually
+    NOTE: the key of the returned dictionary is a positive edge
+    """
+    pos_src = src.cpu().numpy()
+    pos_dst = dst.cpu().numpy()
+
+    batch_size = len(pos_src)
+
+    all_dst = np.arange(min_dst_idx, max_dst_idx + 1)
+
+    edges_per_pos_edge = {}
+    # positive edges
+    for pos_s, pos_d in zip(pos_src, pos_dst):
+        if (pos_s, pos_d) not in edges_per_pos_edge:
+            edges_per_pos_edge[(pos_s, pos_d)] = {'pos': [pos_d]}
+
+    # negative edges
+    for (pos_s, pos_d) in edges_per_pos_edge:
+        edges_per_pos_edge[(pos_s, pos_d)]['neg'] = [neg_dst for neg_dst in all_dst if neg_dst != pos_d]
+
+    return edges_per_pos_edge
+
+
 def eval_hits(y_pred_pos, y_pred_neg, type_info, K):
     '''
         source: https://github.com/snap-stanford/ogb/blob/d5c11d91c9e1c22ed090a2e0bbda3fe357de66e7/ogb/linkproppred/evaluate.py#L214
@@ -57,12 +101,60 @@ def eval_hits(y_pred_pos, y_pred_neg, type_info, K):
         kth_score_in_negative_edges = torch.topk(y_pred_neg, K)[0][-1]
         hitsK = float(torch.sum(y_pred_pos > kth_score_in_negative_edges).cpu()) / len(y_pred_pos)
 
-    # type_info is numpy
     else:
         kth_score_in_negative_edges = np.sort(y_pred_neg)[-K]
         hitsK = float(np.sum(y_pred_pos > kth_score_in_negative_edges)) / len(y_pred_pos)
 
     return hitsK
+
+
+def eval_mrr(y_pred_pos, y_pred_neg, type_info, K):
+    """
+    source: https://github.com/snap-stanford/ogb/blob/d5c11d91c9e1c22ed090a2e0bbda3fe357de66e7/ogb/linkproppred/evaluate.py#L214
+    """
+    if type_info == 'torch':
+        # calculate ranks
+        y_pred_pos = y_pred_pos.view(-1, 1)
+        # optimistic rank: "how many negatives have a larger score than the positive?"
+        # ~> the positive is ranked first among those with equal score
+        optimistic_rank = (y_pred_neg > y_pred_pos).sum(dim=1)
+        # pessimistic rank: "how many negatives have at least the positive score?"
+        # ~> the positive is ranked last among those with equal score
+        pessimistic_rank = (y_pred_neg >= y_pred_pos).sum(dim=1)
+        ranking_list = 0.5 * (optimistic_rank + pessimistic_rank) + 1
+        # hits1_list = (ranking_list <= 1).to(torch.float)
+        # hits3_list = (ranking_list <= 3).to(torch.float)
+        # hits10_list = (ranking_list <= 10).to(torch.float)
+        hitsK_list = (ranking_list <= K).to(torch.float)
+        mrr_list = 1./ranking_list.to(torch.float)
+
+        return {
+                #  'hits@1_list': hits1_list,
+                #  'hits@3_list': hits3_list,
+                #  'hits@10_list': hits10_list,
+                'hits@k': hitsK_list.mean(),
+                'mrr_list': mrr_list.mean()
+                }
+
+    else:
+        y_pred_pos = y_pred_pos.reshape(-1, 1)
+        optimistic_rank = (y_pred_neg >= y_pred_pos).sum(dim=1)
+        pessimistic_rank = (y_pred_neg > y_pred_pos).sum(dim=1)
+        ranking_list = 0.5 * (optimistic_rank + pessimistic_rank) + 1
+        # hits1_list = (ranking_list <= 1).astype(np.float32)
+        # hits3_list = (ranking_list <= 3).astype(np.float32)
+        # hits10_list = (ranking_list <= 10).astype(np.float32)
+        hitsK_list = (ranking_list <= K).astype(np.float32)
+        mrr_list = 1./ranking_list.astype(np.float32)
+
+        return {
+                # 'hits@1_list': hits1_list,
+                # 'hits@3_list': hits3_list,
+                # 'hits@10_list': hits10_list,
+                'hits@k': hitsK_list.mean(),
+                'mrr_list': mrr_list.mean()
+                }
+
 
 
 def metric_at_k_score(y_true, y_pred_proba, k=K, pos_label=1):
@@ -81,11 +173,13 @@ def metric_at_k_score(y_true, y_pred_proba, k=K, pos_label=1):
             reverse=True
         )[:k]
     ]
-    precision_at_k = sum(topk) / len(topk) 
-    recall_at_k = sum(topk) / sum(y_true == pos_label)
-    
-    if precision_at_k + recall_at_k != 0:
-        f1_at_k = 2 * (precision_at_k * recall_at_k) / (precision_at_k + recall_at_k)
+    metrics_at_k = {'precision@k': sum(topk) / len(topk) ,
+                    'recall@k': sum(topk) / sum(y_true == pos_label),
+                    }
+    if metrics_at_k['precision@k'] + metrics_at_k['recall@k'] != 0:
+        f1_at_k = 2 * (metrics_at_k['precision@k'] * metrics_at_k['recall@k']) / (metrics_at_k['precision@k'] + metrics_at_k['recall@k'])
     else:
         f1_at_k = 0
-    return precision_at_k, recall_at_k, f1_at_k
+    metrics_at_k ['f1@k'] = f1_at_k
+
+    return metrics_at_k
