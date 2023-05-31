f799db1417a846ab8247895367fc0f3397095ccc
Farimah Poursafaei <fpour@narval4.narval.calcul.quebec>
2023-05-31 10:21:43 -0400

    small changes

--- tgb/utils/dataset_stats.py
+++ tgb/utils/dataset_stats.py
@@ -5,8 +5,10 @@ Dataset statistics
 import numpy as np
 import pandas as pd
 import networkx as nx
+import argparse
 
 from torch_geometric.loader import TemporalDataLoader
+from tgb.nodeproppred.dataset_pyg import PyGNodePropertyDataset
 from tgb.linkproppred.dataset import LinkPropPredDataset
 
 
@@ -58,11 +60,45 @@ def get_avg_degree(edgelist_df):
     return np.mean(degree_avg_at_ts_list)
 
 
-def get_dataset_stats(data):
+def get_index_stats(train_val_data, test_data):
+    r"""
+    compute `surprise` and `recurrence` indices
+    """
+    train_val_e_set = {}
+    for src, dst in zip(train_val_data['sources'], train_val_data['destinations']):
+        if (src, dst) not in train_val_e_set:
+            train_val_e_set[(src, dst)] = True
+    
+    test_e_set = {}
+    for src, dst in zip(test_data['sources'], test_data['destinations']):
+        if (src, dst) not in test_e_set:
+            test_e_set[(src, dst)] = True
+    
+    train_val_size = len(train_val_data['sources'])
+    test_size = len(test_data['sources'])
+
+    intersect = difference = 0
+    for e in test_e_set:
+        if e in train_val_e_set:
+            intersect += 1
+        else:
+            difference += 1
+
+    surprise = float(difference * 1.0 / test_size)
+    recurrence = float(intersect * 1.0 / train_val_size)
+    return surprise, recurrence
+
+
+def get_dataset_stats(data, task='linkproppred'):
     r"""
     returns simple stats based on counts
     """
-    sources, destinations, timestamps = data['sources'], data['destinations'], data['timestamps']
+    if task == 'linkproppred':
+        sources, destinations, timestamps = data['sources'], data['destinations'], data['timestamps']
+    elif task == 'nodeproppred':
+        sources, destinations, timestamps = data.src, data.dst, data.t
+    else:
+        raise ValueError(f"Task can either be `linkproppred` or `nodeproppred`! {task} not acceptable.")
     edgelist_df = pd.DataFrame(zip(sources, destinations, timestamps), columns=['src', 'dst', 'ts'])
     num_nodes = len(np.unique(np.concatenate((sources, destinations), axis=0)))
     num_edges = len(sources)  # = len(destinations) = len(timestamps)
@@ -78,6 +114,8 @@ def get_dataset_stats(data):
                   'num_unique_e': num_unique_e,
                   'avg_e_per_ts': avg_e_per_ts,
                   'avg_degree_per_ts': avg_degree_per_ts,
+                  'surprise': 0,
+                  'reocurrence': 0,
                   }
     return stats_dict
 
@@ -86,13 +124,51 @@ def main():
     r"""
     Generate dateset statistics
     """
-    DATA = "stablecoin"
+    parser = argparse.ArgumentParser(description='Dataset statistics')
+    parser.add_argument('-d', '--data', type=str, default='wikipedia', help='random seed to use')
+    parser.parse_args()
+    args = parser.parse_args()
 
-    # load data
-    dataset = LinkPropPredDataset(name=DATA, root="datasets", preprocess=True)
-    data = dataset.full_data   
+    DATA = args.data
+    task = ''
 
-    dataset_stats = get_dataset_stats(data)
+    if DATA in ['wikipedia', 'amazonreview', 'opensky', 'redditcomments', 'stablecoin']:
+        task = 'linkproppred'
+
+        # load data: link prop. pred
+        dataset = LinkPropPredDataset(name=DATA, root="datasets", preprocess=True)
+        data = dataset.full_data   
+
+        # split data
+        train_mask = dataset.train_mask
+        val_mask = dataset.val_mask
+        test_mask = dataset.test_mask
+        temporal_data = dataset.get_TemporalData()
+        
+        train_val_mask = np.logical_or(train_mask, val_mask)
+        train_val_data = temporal_data[train_val_mask]
+        test_data = temporal_data[test_mask]
+
+    elif DATA in ['un_trade', 'lastfmgenre', 'subreddits']:
+        task = 'nodeproppred'
+
+        # load data: node prop. pred.
+        dataset = PyGNodePropertyDataset(name=DATA, root="datasets")
+        data = dataset.get_TemporalData()
+        
+        # split data
+        train_mask = dataset.train_mask
+        val_mask = dataset.val_mask
+        test_mask = dataset.test_mask
+
+        train_val_mask = np.logical_or(train_mask, val_mask)
+        train_val_data = data[train_val_mask]
+        test_data = data[test_mask]
+
+    else:
+        raise ValueError("Unsupported data!")
+
+    dataset_stats = get_dataset_stats(data, train_val_data, test_data, task)
     print(f"DATA: {DATA}")
     for k, v in dataset_stats.items():
         print(f"{k}: {v}")
