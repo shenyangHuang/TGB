154f1b1b705fecf1971261e6320d38547a519c50
shenyangHuang <shenyang.huang@mail.mcgill.ca>
2024-02-26 21:31:32 -0500

    updating data extraction script

--- tgb/datasets/ICEWS14/icews14.py
+++ tgb/datasets/ICEWS14/icews14.py
@@ -11,18 +11,41 @@ def load_index(input_path):
 
 
 def load_tab_list(input_path):
+    rows = []
     with open(input_path) as f:
         for i, line in enumerate(f.readlines()): 
-            u,v,e,t, = line.strip().split("\t")
+            head,relation,tail,t, = line.strip().split("\t")
+            rows.append([t,head,tail,relation])
+    return rows
 
         
+def write2csv(rows, output_path):
+    with open(output_path, "w") as f:
+        writer = csv.writer(f)
+        writer.writerow(["timestamp", "head", "tail", "relation_type"])
+        writer.writerows(rows)
+
 
 def main():
     """
     concatenate and merge the edgelists into one 
     change tab to ,
     """
-    fname = "train.txt"
+    train_name = "train.txt"
+    train_rows = load_tab_list(train_name)
+
+    val_name = "valid.txt"
+    val_rows = load_tab_list(val_name)
+
+    test_name = "test.txt"
+    test_rows = load_tab_list(test_name)
+
+    all_rows = train_rows + val_rows + test_rows
+    output_path = "icews14.csv"
+    write2csv(all_rows, output_path)
+
+
+
 
 
 
