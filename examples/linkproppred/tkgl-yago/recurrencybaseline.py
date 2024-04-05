8eb5a826f4a71ad6210258bd186c05b5580b0c0d
JuliaGast <julia.gastinger@yahoo.de>
2024-04-05 15:31:52 +0200

    running but slow version of baseline. produces correct results

--- examples/linkproppred/tkgl-yago/recurrencybaseline.py
+++ examples/linkproppred/tkgl-yago/recurrencybaseline.py
@@ -1,148 +1,3 @@
-'''
-       Recurrency Baseline
-	  
-  File:     recurrencybaseline.py
-  Authors:  Julia Gastinger (julia.gastinger@neclab.eu)
-
-NEC Laboratories Europe GmbH, Copyright (c) 2024, All rights reserved.  
-
-       THIS HEADER MAY NOT BE EXTRACTED OR MODIFIED IN ANY WAY.
- 
-       PROPRIETARY INFORMATION ---  
-
-SOFTWARE LICENSE AGREEMENT
-
-ACADEMIC OR NON-PROFIT ORGANIZATION NONCOMMERCIAL RESEARCH USE ONLY
-
-BY USING OR DOWNLOADING THE SOFTWARE, YOU ARE AGREEING TO THE TERMS OF THIS
-LICENSE AGREEMENT.  IF YOU DO NOT AGREE WITH THESE TERMS, YOU MAY NOT USE OR
-DOWNLOAD THE SOFTWARE.
-
-This is a license agreement ("Agreement") between your academic institution
-or non-profit organization or self (called "Licensee" or "You" in this
-Agreement) and NEC Laboratories Europe GmbH (called "Licensor" in this
-Agreement).  All rights not specifically granted to you in this Agreement
-are reserved for Licensor. 
-
-RESERVATION OF OWNERSHIP AND GRANT OF LICENSE: Licensor retains exclusive
-ownership of any copy of the Software (as defined below) licensed under this
-Agreement and hereby grants to Licensee a personal, non-exclusive,
-non-transferable license to use the Software for noncommercial research
-purposes, without the right to sublicense, pursuant to the terms and
-conditions of this Agreement. NO EXPRESS OR IMPLIED LICENSES TO ANY OF
-LICENSOR'S PATENT RIGHTS ARE GRANTED BY THIS LICENSE. As used in this
-Agreement, the term "Software" means (i) the actual copy of all or any
-portion of code for program routines made accessible to Licensee by Licensor
-pursuant to this Agreement, inclusive of backups, updates, and/or merged
-copies permitted hereunder or subsequently supplied by Licensor,  including
-all or any file structures, programming instructions, user interfaces and
-screen formats and sequences as well as any and all documentation and
-instructions related to it, and (ii) all or any derivatives and/or
-modifications created or made by You to any of the items specified in (i).
-
-CONFIDENTIALITY/PUBLICATIONS: Licensee acknowledges that the Software is
-proprietary to Licensor, and as such, Licensee agrees to receive all such
-materials and to use the Software only in accordance with the terms of this
-Agreement.  Licensee agrees to use reasonable effort to protect the Software
-from unauthorized use, reproduction, distribution, or publication. All
-publication materials mentioning features or use of this software must
-explicitly include an acknowledgement the software was developed by NEC
-Laboratories Europe GmbH.
-
-COPYRIGHT: The Software is owned by Licensor.  
-
-PERMITTED USES:  The Software may be used for your own noncommercial
-internal research purposes. You understand and agree that Licensor is not
-obligated to implement any suggestions and/or feedback you might provide
-regarding the Software, but to the extent Licensor does so, you are not
-entitled to any compensation related thereto.
-
-DERIVATIVES: You may create derivatives of or make modifications to the
-Software, however, You agree that all and any such derivatives and
-modifications will be owned by Licensor and become a part of the Software
-licensed to You under this Agreement.  You may only use such derivatives and
-modifications for your own noncommercial internal research purposes, and you
-may not otherwise use, distribute or copy such derivatives and modifications
-in violation of this Agreement.
-
-BACKUPS:  If Licensee is an organization, it may make that number of copies
-of the Software necessary for internal noncommercial use at a single site
-within its organization provided that all information appearing in or on the
-original labels, including the copyright and trademark notices are copied
-onto the labels of the copies.
-
-USES NOT PERMITTED:  You may not distribute, copy or use the Software except
-as explicitly permitted herein. Licensee has not been granted any trademark
-license as part of this Agreement.  Neither the name of NEC Laboratories
-Europe GmbH nor the names of its contributors may be used to endorse or
-promote products derived from this Software without specific prior written
-permission.
-
-You may not sell, rent, lease, sublicense, lend, time-share or transfer, in
-whole or in part, or provide third parties access to prior or present
-versions (or any parts thereof) of the Software.
-
-ASSIGNMENT: You may not assign this Agreement or your rights hereunder
-without the prior written consent of Licensor. Any attempted assignment
-without such consent shall be null and void.
-
-TERM: The term of the license granted by this Agreement is from Licensee's
-acceptance of this Agreement by downloading the Software or by using the
-Software until terminated as provided below.  
-
-The Agreement automatically terminates without notice if you fail to comply
-with any provision of this Agreement.  Licensee may terminate this Agreement
-by ceasing using the Software.  Upon any termination of this Agreement,
-Licensee will delete any and all copies of the Software. You agree that all
-provisions which operate to protect the proprietary rights of Licensor shall
-remain in force should breach occur and that the obligation of
-confidentiality described in this Agreement is binding in perpetuity and, as
-such, survives the term of the Agreement.
-
-FEE: Provided Licensee abides completely by the terms and conditions of this
-Agreement, there is no fee due to Licensor for Licensee's use of the
-Software in accordance with this Agreement.
-
-DISCLAIMER OF WARRANTIES:  THE SOFTWARE IS PROVIDED "AS-IS" WITHOUT WARRANTY
-OF ANY KIND INCLUDING ANY WARRANTIES OF PERFORMANCE OR MERCHANTABILITY OR
-FITNESS FOR A PARTICULAR USE OR PURPOSE OR OF NON- INFRINGEMENT.  LICENSEE
-BEARS ALL RISK RELATING TO QUALITY AND PERFORMANCE OF THE SOFTWARE AND
-RELATED MATERIALS.
-
-SUPPORT AND MAINTENANCE: No Software support or training by the Licensor is
-provided as part of this Agreement.  
-
-EXCLUSIVE REMEDY AND LIMITATION OF LIABILITY: To the maximum extent
-permitted under applicable law, Licensor shall not be liable for direct,
-indirect, special, incidental, or consequential damages or lost profits
-related to Licensee's use of and/or inability to use the Software, even if
-Licensor is advised of the possibility of such damage.
-
-EXPORT REGULATION: Licensee agrees to comply with any and all applicable
-export control laws, regulations, and/or other laws related to embargoes and
-sanction programs administered by law.
-
-SEVERABILITY: If any provision(s) of this Agreement shall be held to be
-invalid, illegal, or unenforceable by a court or other tribunal of competent
-jurisdiction, the validity, legality and enforceability of the remaining
-provisions shall not in any way be affected or impaired thereby.
-
-NO IMPLIED WAIVERS: No failure or delay by Licensor in enforcing any right
-or remedy under this Agreement shall be construed as a waiver of any future
-or other exercise of such right or remedy by Licensor.
-
-GOVERNING LAW: This Agreement shall be construed and enforced in accordance
-with the laws of Germany without reference to conflict of laws principles.
-You consent to the personal jurisdiction of the courts of this country and
-waive their rights to venue outside of Germany.
-
-ENTIRE AGREEMENT AND AMENDMENTS: This Agreement constitutes the sole and
-entire agreement between Licensee and Licensor as to the matter set forth
-herein and supersedes any previous agreements, understandings, and
-arrangements between the parties relating hereto.
-
-       THIS HEADER MAY NOT BE EXTRACTED OR MODIFIED IN ANY WAY.
-'''
 
 import sys
 sys.path.insert(0, '/home/jgastinger/tgb/TGB2')
@@ -234,10 +89,8 @@ def test(best_config, basis_dict, rels, num_nodes, num_rels, test_data_prel, all
 
                 # updates the scores and logging dict for each process
                 for proc_loop in range(num_processes_tmp):
-                    scores_dict_for_test.update(output[proc_loop][1])
-                    final_logging_dict.update(output[proc_loop][0])
-                    perf_list_all.extend(output[proc_loop][2])
-                    hits_list_all.extend(output[proc_loop][3])
+                    perf_list_all.extend(output[proc_loop][0])
+                    hits_list_all.extend(output[proc_loop][1])
 
             ## use this if you dont wanna use ray:
             else:
@@ -246,7 +99,7 @@ def test(best_config, basis_dict, rels, num_nodes, num_rels, test_data_prel, all
                                         num_nodes, num_rels, 
                                         lmbda_psi, alpha, neg_samples_batch,pos_samples_batch, 
                                         evaluator)
-                scores_dict_for_test, final_logging_dict, perf_list, hits_list = output
+                perf_list, hits_list = output
                 perf_list_all.extend(perf_list)
                 hits_list_all.extend(hits_list)
             
@@ -305,7 +158,7 @@ def train(params_dict, basis_dict, rels, num_nodes, num_rels, val_data_prel, tra
             lmbdas_psi = params_dict['lmbda_psi']        
 
             alpha = 1
-            best_lmbda_psi = 0
+            best_lmbda_psi = 0.1
             best_mrr_psi = 0
             lmbda_mrrs = []
 
@@ -331,10 +184,8 @@ def train(params_dict, basis_dict, rels, num_nodes, num_rels, val_data_prel, tra
 
                 # updates the scores and logging dict for each process
                 for proc_loop in range(num_processes_tmp):
-                    # scores_dict_for_eval_lambda.update(output[proc_loop][1])
-                    # final_logging_dict.update(output[proc_loop][0])
-                    perf_list_all.extend(output[proc_loop][2])
-                    hits_list_all.extend(output[proc_loop][3])
+                    perf_list_all.extend(output[proc_loop][0])
+                    hits_list_all.extend(output[proc_loop][1])
 
                 # compute mrr
                 mrr = np.mean(perf_list_all)
@@ -355,6 +206,7 @@ def train(params_dict, basis_dict, rels, num_nodes, num_rels, val_data_prel, tra
             alpha_mrrs = []
             # perf_list_all = []
             best_mrr_alpha = 0
+            best_alpha=0.99
             for alpha in alphas:
                 perf_list_all = []
                 hits_list_all = []
@@ -372,8 +224,8 @@ def train(params_dict, basis_dict, rels, num_nodes, num_rels, val_data_prel, tra
                 for proc_loop in range(num_processes_tmp):
                     # scores_dict_for_eval_lambda.update(output[proc_loop][1])
                     # final_logging_dict.update(output[proc_loop][0])
-                    perf_list_all.extend(output[proc_loop][2])
-                    hits_list_all.extend(output[proc_loop][3])
+                    perf_list_all.extend(output[proc_loop][0])
+                    hits_list_all.extend(output[proc_loop][1])
 
                 # compute mrr
                 mrr_alpha = np.mean(perf_list_all)
@@ -434,7 +286,7 @@ def get_args():
     parser.add_argument("--window", "-w", default=0, type=int) # set to e.g. 200 if only the most recent 200 timesteps should be considered. set to -2 if multistep
     parser.add_argument("--num_processes", "-p", default=1, type=int)
     parser.add_argument("--lmbda", "-l",  default=0.1, type=float) # fix lambda. used if trainflag == false
-    parser.add_argument("--alpha", "-alpha",  default=0.999, type=float) # fix alpha. used if trainflag == false
+    parser.add_argument("--alpha", "-alpha",  default=0.99, type=float) # fix alpha. used if trainflag == false
     parser.add_argument("--train_flag", "-tr",  default=False) # do we need training, ie selection of lambda and alpha
     parser.add_argument("--save_config", "-c",  default=True) # do we need to save the selection of lambda and alpha in config file?
     parser.add_argument('--seed', type=int, help='Random seed', default=1)
@@ -551,7 +403,7 @@ Path(results_path).mkdir(parents=True, exist_ok=True)
 results_filename = f'{results_path}/{MODEL_NAME}_NONE_{DATA}_results.json'
 
 
-metric = name.eval_metric
+metric = dataset.eval_metric
 save_results({'model': MODEL_NAME,
               'train_flag': parsed['train_flag'],
               'data': DATA,
