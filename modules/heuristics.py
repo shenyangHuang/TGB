a9509d154e435999a07289391955b97fac71f5d6
e-izdfr <emad.izadifar@gmail.com>
2024-06-01 15:11:48 +0330

    Defined cumulative moving average class

--- modules/heuristics.py
+++ modules/heuristics.py
@@ -47,6 +47,31 @@ class MovingAverage:
         else:
             return np.zeros(self.num_class)
         
+
+class CumulativeMovingAverage:
+    def __init__(self, num_class):
+        self.dict = {}
+        self.num_class = num_class
+
+    def update_dict(self, node_id, node_time, label):
+        if node_id in self.dict:
+            total = self.dict[node_id] * (node_time - 1987) + label
+            self.dict[node_id] = total / (node_time - 1987 + 1)
+        else:
+            self.dict[node_id] = label
+            
+    def query_dict(self, node_id):
+        r"""
+        Parameters:
+            node_id: the node to query
+        Returns:
+            returns the last seen label of the node if it exists, if not return zero vector
+        """
+        if node_id in self.dict:
+            return self.dict[node_id]
+        else:
+            return np.zeros(self.num_class)
+        
 class ExponentialMovingAverage:
     def __init__(self, num_class, alpha=0.1):
         self.dict = {}
